b018e98e307e5eadb6a70c810f07ef0bbb2a9eff
saitejarageti <saitejarageti129@gmail.com>
2025-07-07 19:37:09 +0530

    terraform

--- 20.sg/main.tf
+++
@@ -1,9 +0,0 @@
-module "frontend" {
-    source = "git::https://github.com/SaitejaRageti/terraform-aws-securitygroup.git?ref=main"
-    project = var.project
-    environment = var.environment
-    sg_description = var.sg_description
-    sg_tags = var.sg_tags
-    sg_name = var.sg_name
-    vpc_id = local.vpc_id
-}
--- destroy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# cd 00.vpc ; terraform destroy -auto-approve 
-
-# cd ../1o-sg ; terraform destroy -auto-approve
-
-for i in 00.vpc 10-sg 20-bastion 50-backend-alb ;
-    do cd $i/ ; terraform apply -auto-approve ; cd ..
-done
